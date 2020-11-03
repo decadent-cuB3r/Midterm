560e9533426cce88753a205ee9c9a071ddf3fd2c
decadent-cub3r <cub3r.ezra.yu@gmail.com>
2020-11-04 00:33:01 +0800

    LOGO_href

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.image').hide().slideDown(3000);
+    $('.aside').hide().slideDown(3000);
+    $('.intro').hide().slideDown(3000);
+    $('.video_wrapping').hide().slideDown(3000);
+});
