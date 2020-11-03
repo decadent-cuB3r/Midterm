f9da989905842333b895f0aeafed36da60b13746
decadent-cub3r <cub3r.ezra.yu@gmail.com>
2020-11-04 02:38:19 +0800

    rules js

--- js/rules.js
+++ js/rules.js
@@ -1,15 +1,27 @@
-$(document).ready(function(){
-    $('.Q_btn').click(function(){
-        var Q_btn_id = $(this).attr('data-Aid');
-        $('#'+Q_btn_id).slideToggle(500)
-        $(this).css({
+$(document).ready(function () {
+    function click_tri() {
+        $('Q_btn').css({
             "margin-right": "30px",
             /* 三角形產生器 */
             width: "0px",
             height: "0px",
             "border-left": "20px solid #FF8C4B",
             "border-right": "20px solid #FF8C4B",
-            "border-bottom": "30px solid #FFEFC7"
+            "border-top": "30px solid #FFEFC7"
+        }).click(function () {
+            $('Q_btn').animate({
+                "margin-right": "30px",
+                /* 三角形產生器 */
+                width: "0px",
+                height: "0px",
+                "border-left": "20px solid #FF8C4B",
+                "border-right": "20px solid #FF8C4B",
+                "border-bottom": "30px solid #FFEFC7"
+            }, 1000, click_tri())
         })
+    }
+    $('.Q_btn').click(function () {
+        var Q_btn_id = $(this).attr('data-Aid');
+        $('#' + Q_btn_id).slideToggle(500)
     })
 });
