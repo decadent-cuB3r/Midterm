f05f3d1e5190f38ce24d6e98f73a8b638056cdf7
decadent-cub3r <cub3r.ezra.yu@gmail.com>
2020-11-04 11:28:25 +0800

    try

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.text').hide().fadeIn(2000);
+
+    //init scrollMagic
+    var controller = new ScrollMagic.Controller();
+
+    //create a scene
+    var ourScene = new ScrollMagic.Scene({
+        triggerElement = '#intro'
+    })
+    .setClassToggle('#intro', 'fadeIn').addTo(controller);
+
 });
